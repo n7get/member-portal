2f13875b469952d6c6a15c1f1608928c07c0e107
Robert Ambrose <rna@muttsoft.com>
2023-09-01 14:46:19 -0700

    Tidied up routes in web.php again.

--- routes/web.php
+++ routes/web.php
@@ -26,39 +26,25 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
+require __DIR__.'/auth.php';
+
+Route::middleware('auth')->resource('capabilities', CapabilityController::class);
+Route::middleware('auth')->resource('certifications', CertificationController::class);
+Route::middleware('auth')->resource('others', OtherController::class);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
-
-Route::middleware('auth')->controller(MemberController::class)->group(function() {
-    Route::get('members', 'index')->name('members.index');
-    Route::post('members', 'store')->name('members.store');
+Route::middleware('auth')->controller(MemberController::class)->group(function () {
     Route::get('members/cancel', 'cancel')->name('members.cancel');
     Route::get('members/create/{user}', 'create')->name('members.create-for-user');
-    Route::get('members/create', 'create')->name('members.create');
-    Route::get('members/{member}', 'show')->name('members.show');
-    Route::put('members/{member}', 'update')->name('members.update');
-    Route::delete('members/{member}', 'destroy')->name('members.destroy');
-    Route::get('members/{member}/edit', 'edit')->name('members.edit');
-    Route::get('members/cancel', 'cancel')->name('members.cancel');
 });
-
-Route::middleware('auth')->resource('capabilities', CapabilityController::class);
-Route::middleware('auth')->resource('certifications', CertificationController::class);
-Route::middleware('auth')->resource('others', OtherController::Class);
+Route::middleware('auth')->resource('members', MemberController::class);
 
 Route::middleware('auth')->controller(UserController::class)->group(function () {
-    Route::get('users', 'index')->name('users.index');
-    Route::post('users', 'store')->name('users.store');
-    Route::get('users/cancel', 'cancel')->name('users.cancel');
-    Route::get('users/create', 'create')->name('users.create');
-    Route::get('users/{user}', 'show')->name('users.show');
-    Route::put('users/{user}', 'update')->name('users.update');
-    Route::delete('users/{user}', 'destroy')->name('users.destroy');
-    Route::get('users/{user}/edit', 'edit')->name('users.edit');
     Route::get('users/cancel', 'cancel')->name('users.cancel');
 });
+Route::middleware('auth')->resource('users', UserController::class);
