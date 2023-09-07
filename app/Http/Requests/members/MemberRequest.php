d83f39fc57369d152e144c8393e1233a9758b89f
Robert Ambrose <rna@muttsoft.com>
2023-09-07 09:14:12 -0700

    Fixed mailing address validation so either all or none inputs are required.

--- app/Http/Requests/members/MemberRequest.php
+++ app/Http/Requests/members/MemberRequest.php
@@ -21,24 +21,32 @@ class MemberRequest extends FormRequest
      */
     public function rules(): array
     {
-        $callsignRule = ['required', 'max:7'];
-        if(request()->routeIs('members.store')) {
-            array_push($callsignRule, 'unique:members');
-        }
-
-        return [
+        $rules = [
             'first_name' => ['required', 'max:100'],
             'last_name' => ['required', 'max:100'],
             'mailing_address_street' => ['max:100'],
             'mailing_address_city' => ['max:100'],
             'mailing_address_state' => ['max:2'],
             'mailing_address_zip' => ['max:10'],
-            'callsign' => $callsignRule,
+            'callsign' => ['required', 'max:7'],
             'expiration' => ['required', 'date'],
             'shares_callsign' => ['max:10'],
             'gmrs_callsign' => ['max:10'],
             'cellPhone' => ['required', 'max:20'],
             'cell_sms_carrier' => ['required', 'max:20'],
         ];
+
+        if (request()->routeIs('members.store')) {
+            array_push($rules['callsign'], 'unique:members');
+        }
+
+        if ($this->mailing_address_street || $this->mailing_address_city || $this->mailing_address_state || $this->mailing_address_zip) {
+            array_unshift($rules['mailing_address_street'], 'required');
+            array_unshift($rules['mailing_address_city'], 'required');
+            array_unshift($rules['mailing_address_state'], 'required');
+            array_unshift($rules['mailing_address_zip'], 'required');
+        }
+
+        return $rules;
     }
 }
