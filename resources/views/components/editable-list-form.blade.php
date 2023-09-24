c2e1eb02c58925672afc24960cfe610c3a773570
Robert Ambrose <rna@muttsoft.com>
2023-09-24 14:24:25 -0700

    Added add button.

--- resources/views/components/editable-list-form.blade.php
+++ resources/views/components/editable-list-form.blade.php
@@ -8,7 +8,7 @@
   {{ method_field('PUT') }}
   {{ $slot }}
   <div class="flex gap-3 justify-end pt-6">
-    <x-secondary-button x-data="" x-on:click="$dispatch('add-item')">{{ $add }}</x-secondary-button>
+    <x-add-button x-data="" x-on:click="$dispatch('add-item')">{{ $add }}</x-add-button>
     <x-primary-button type="submit">Save</x-primary-button>
   </div>
 </form>
