770d75507052dbd3455366bcbd7035e488eab4cc
Robert Ambrose <rna@muttsoft.com>
2023-10-12 10:17:27 -0700

    Fixed Roles column wrapping in User index view.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,8 +8,8 @@
       <div class="panel">
         <div class="border-b-2 sm:border-none mx-2 hidden sm:flex gap-2">
         <div class="w-11/12 flex gap-2">
-          <div class="w-1/12 font-bold underline">Roles</div>
-          <div class="w-5/12 font-bold underline">Email</div>
+          <div class="w-2/12 font-bold underline">Roles</div>
+          <div class="w-4/12 font-bold underline">Email</div>
           <div class="w-4/12 font-bold underline">Name</div>
           <div class="w-2/12 font-bold underline">Callsign</div>
         </div>
@@ -18,14 +18,14 @@
       @foreach($users as $user)
         <div class="border-b-2 sm:border-none mx-2 mt-2 sm:mt-0 pb-2 sm:pb-0 flex gap-2 hover:bg-gray-100"">
           <div class="w-4/5 sm:w-11/12 sm:flex gap-2">
-            <div class="w-1/12 text-sm sm:text-lg">
+            <div class="w-2/12 text-sm sm:text-lg">
               {{ $user->hasRole('admin') ? 'A' : '' }}
               {{ $user->hasRole('activities') ? 'C' : '' }}
               {{ $user->hasRole('leadership') ? 'L' : '' }}
               {{ $user->hasRole('member') ? 'M' : '' }}
               {{ $user->hasRole('resources') ? 'R' : '' }}
             </div>
-            <div class="w-5/12 sm:truncate font-extrabold sm:font-normal">
+            <div class="w-4/12 sm:truncate font-extrabold sm:font-normal">
               <a class="link" href="{{ route('users.show', $user->id) }}">{{ $user->email }}</a>
             </div>
             <div class="w-4/12 text-sm sm:text-lg sm:truncate">{{ $user->name() }}</div>
